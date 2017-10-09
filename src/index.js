788e8981eda4bd07c826fbf36db1a92c884da361
KM Tu <me@kmtu.info>
2017-10-09 17:35:50 +0900

    Wrapping up for the next guide

--- src/index.js
+++ src/index.js
@@ -1,31 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg';
-import Data from './data.xml';
 
 function component() {
   let element = document.createElement('div');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div
-  let myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  let credit = document.createElement('p');
-  let creditLink = document.createElement('a');
-  let txtNode = document.createTextNode(' by Icon8');
-  creditLink.href = 'https://icons8.com/icon/12244/Idea';
-  creditLink.textContent = 'Idea icon';
-  credit.appendChild(creditLink);
-  credit.appendChild(txtNode);
-
-  element.appendChild(credit);
-
-  console.log(Data);
 
   return element;
 }
