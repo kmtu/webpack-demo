1b9bc06e23304b94916db15d7c64268326fd2b86
KM Tu <me@kmtu.info>
2017-10-09 22:21:03 +0900

    Setup webpack-dev-middleware

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
     })
   ],
   output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, distName)
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, distName),
+    publicPath: '/'
   },
 };
