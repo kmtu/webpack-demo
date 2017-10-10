8d3c3ac4312adbb2d753b2cfc4994b255bb9f226
KM Tu <me@kmtu.info>
2017-10-10 16:22:49 +0900

    Preparation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import _ from 'lodash';
+import {cube} from './math.js';
 
 function component() {
-  let element = document.createElement('div');
+  let element = document.createElement('pre');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = [
+    'Hello webpack!',
+    `5 cube is equal to ${cube(5)}`
+  ].join('\n\n');
 
   return element;
 }
