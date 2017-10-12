bde21c1598487c37548d56737ef2cd97987cecb0
KM Tu <me@kmtu.info>
2017-10-12 22:11:29 +0900

    Lazy loading

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-async function getComponent() {
+import _ from 'lodash';
+
+function component() {
   let element = document.createElement('div');
-  try {
-    const _ = await import(/* webpackChunkName: "lodash" */ 'lodash')
-  }
-  catch (error) {
-    console.log('An error occurred while loading the component');
-  }
+  let button = document.createElement('button');
+  let br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console!';
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    let print = module.default;
+    print();
+  });
+
   return element;
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component);
-})
-
+document.body.appendChild(component());
